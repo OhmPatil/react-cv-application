1592c7f2510eb8ba22c523048683289649de2613
OhmPatil <workpatil10@gmail.com>
2022-07-22 03:10:52 +0530

    Add logic to add/delete education form

--- src/components/EducationInfo.js
+++ src/components/EducationInfo.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 // import Form from "./Form";
+import uniqid from 'uniqid'
 
 class EducationInfo extends Component {
     constructor(props){
@@ -11,21 +12,33 @@ class EducationInfo extends Component {
                 uniname: '',
                 city: '',
                 from: '',
-                to: ''
-            },
-            {
+                to: '',
+                id: uniqid()
+            }],
+
+            education: {
                 degree: '',
                 uniname: '',
                 city: '',
                 from: '',
-                to: ''
-            }]
+                to: '',
+                id: uniqid()
+            }
         }
     }
 
     handleChange = (e) => {
         // this.setState({degree: e.target.value})
-        console.log(this.state.educations);
+        // console.log(this.state.educations);
+        this.setState({
+            education: {
+                degree: e.target.value,
+                uniname: e.target.value,
+                city: e.target.value,
+                from: e.target.value,
+                to: e.target.value,
+            }
+        })
     }
 
     displayEducationForm = (e) => {
@@ -56,32 +69,47 @@ class EducationInfo extends Component {
 
     }
 
+    handleAddEducation = () => {
+        this.setState({
+            educations: this.state.educations.concat(this.state.education),
+            education: {
+                degree: '',
+                uniname: '',
+                city: '',
+                from: '',
+                to: '',
+                id: uniqid()
+            }
+        })
+    }
+
+    handleDeleteEducation = (index) => {
+        const educationsList = [...this.state.educations]
+        educationsList.splice(index, 1)
+        this.setState({educations: educationsList})
+    }
+
     render(){
+
+        // const {educations, education} = this.state
         return(
             <>
-                {/* <div className="subtitle">Education</div>
-                <input onChange={this.handleChange} type='text' placeholder="Degree..." className="form-degree"/>
-                <input onChange={this.handleChange} type='text' placeholder="University name..." id="form-uniname"/>
-                <input onChange={this.handleChange} type='text' placeholder="City..." id="form-city"/>
-                <input onChange={this.handleChange} type='text' placeholder="From..." id="form-from"/>
-                <input onChange={this.handleChange} type='text' placeholder="To..." id="form-to"/>
-                <button onClick={this.displayEducationForm}>Add Education</button> */}
             {this.state.educations.map((education, index) => (
-                <>
-                <div className="subtitle" key={index}>Add Education</div>
+                <div className="edu-form" key={index}>
+                <div className="subtitle" >Add Education</div>
                 <input onChange={this.handleChange} type='text' placeholder="Degree..." className="form-degree"/>
                 <input onChange={this.handleChange} type='text' placeholder="University name..." id="form-uniname"/>
                 <input onChange={this.handleChange} type='text' placeholder="City..." id="form-city"/>
                 <input onChange={this.handleChange} type='text' placeholder="From..." id="form-from"/>
                 <input onChange={this.handleChange} type='text' placeholder="To..." id="form-to"/>
                 {this.state.educations.length - 1 === index && (
-                    <button onClick={this.displayEducationForm}>Add Education</button>
+                    <button onClick={this.handleAddEducation}>Add Education</button>
                 )}
 
                 {this.state.educations.length > 1 && (
-                    <button onClick={this.displayEducationForm}>Delete Education</button>
+                    <button onClick={() => this.handleDeleteEducation(index)}>Delete Education</button>
                 )}
-                </>
+                </div>
                 
             ))}
             </>
